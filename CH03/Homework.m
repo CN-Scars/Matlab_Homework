4c7ea31db257636f1a39a29772913f59d337f1f5
CN-Scars <yss2813483011xxl@gmail.com>
2023-07-10 17:36:10 +0800

    Animate the iterative process and plot the convergence curve

--- CH03/Homework.m
+++ CH03/Homework.m
@@ -7,6 +7,7 @@ pset = zeros(num_iterations, num_teams, 2);  % 粒子集
 pset(1, :, :) = rand(num_teams, 2)*6 - 3;  % 初始位置
 water_level = zeros(num_iterations, num_teams);  % 水位
 velocity = zeros(num_teams, 2);  % 初始速度为0
+gbest_value = zeros(num_iterations, 1);  % 全局最优解的值
 
 % 计算初始水位
 for i = 1:num_teams
@@ -17,10 +18,18 @@ end
 pbest = squeeze(pset(1, :, :));
 gbest_idx = find(water_level(1, :) == max(water_level(1, :)), 1);
 gbest = pbest(gbest_idx, :);
+gbest_value(1) = water_level(1, gbest_idx);  % 存储全局最优解的值
 
 w = 0.3;  % 惯性权重
 c1 = 0.5;  % 参数c1
 c2 = 0.7;  % 参数c2
+
+% 绘制初始3D图
+figure;
+[X,Y] = meshgrid(-3:0.1:3);
+Z = peaks(X,Y);
+mesh(X,Y,Z);
+hold on;
 
 % 迭代
 for t = 2:num_iterations
@@ -42,36 +51,26 @@ for t = 2:num_iterations
         end
     end
     % 更新全局最优
-    [a, b] = find(water_level(1 : t, :) == max(max(water_level(1 : t, :))), 1);
-    gbest = squeeze(pset(a, b, :));
-end
+    [max_value, max_idx] = max(water_level(t, :));
+    if max_value > gbest_value(t-1)
+        gbest_value(t) = max_value;
+        gbest = squeeze(pset(t, max_idx, :));
+    else
+        gbest_value(t) = gbest_value(t-1);
+    end
 
-% 线条和标记样式
-line_styles = {'-', '--', ':', '-.'};
-marker_styles = {'o', '+', '*', '.'};
-
-% 绘图
-figure; % 5个子图
-subplot(num_teams+1, 1, 1);
-plot(max(water_level, [], 2), 'k-^');
-title('找到的最高水位');
-xlabel('迭代次数');
-ylabel('水位');
-for i = 1:num_teams
-    subplot(num_teams+1, 1, i+1);
-    plot(water_level(:, i), line_styles{mod(i-1,4)+1}, 'Marker', marker_styles{mod(i-1,4)+1});
-    title(['团队 ', num2str(i)]);
-    xlabel('迭代次数');
-    ylabel('水位');
+    % 更新3D图
+    clf;  % 清除当前figure窗口
+    mesh(X,Y,Z);
+    hold on;
+    plot3(pset(t,:,1), pset(t,:,2), water_level(t,:), 'r*');
+    pause(0.02);
 end
 
 figure; % 合成图
 hold on;
-plot(max(water_level, [], 2), 'k-^', 'DisplayName','最高水位');
-for i = 1:num_teams
-    plot(water_level(:, i), line_styles{mod(i-1,4)+1}, 'Marker', marker_styles{mod(i-1,4)+1}, 'DisplayName',['团队 ', num2str(i)]);
-end
-title('团队搜寻水位变化');
+plot(gbest_value, 'k-^', 'DisplayName','全局最优水位');
+title('全局最优水位变化');
 xlabel('迭代次数');
 ylabel('水位');
 legend('Location','eastoutside'); % 显示图例，并放到图形的外右边
