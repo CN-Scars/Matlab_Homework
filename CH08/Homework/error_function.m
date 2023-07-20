66422aace260768ad491868b125825bbaa43ad32
CN-Scars <yss2813483011xxl@gmail.com>
2023-07-20 16:29:53 +0800

    Realize the search of control gain parameters by using the PSO algorithm to output the best solution for the simulated discrete system

---
+++ CH08/Homework/error_function.m
@@ -0,0 +1,38 @@
+function error = error_function(EmuD)
+    % 解构EmuD
+    EmuD1 = EmuD(1);
+    EmuD2 = EmuD(2);
+
+    % 初始参数
+    A = [2, 1, 1; 
+         1, 1, 0;
+         0, 1, 2];
+    B = [1, 2;
+         3, 1;
+         1, 1];
+    C = [1, 0, 0;
+         0, 0, 1];
+    x = zeros(3, 1);
+    r = [1; 1];
+    T = 0.01;
+    num_steps = 1000;
+
+    % 计算反馈增益K 和 参考增益F
+    [K, F] = Axui_FeedbackGain(EmuD1, EmuD2);
+
+    % 用于存储每个时间步的输出
+    y = zeros(2, num_steps);
+
+    % 模拟
+    for k = 1:num_steps
+        % 使用控制器计算输入
+        u = -K*x + F*r;
+        % 更新系统状态
+        x = T * (A * x + B * u) + x;
+        % 计算并存储输出
+        y(:, k) = C * x;
+    end
+
+    % 计算总误差
+    error = sum(abs(y(1, :) - 1)) + sum(abs(y(2, :) - 1));
+end
--- CH08/Homework/main.m
+++ CH08/Homework/main.m
@@ -1,4 +1,23 @@
+clc; clear all; close all;
+
 % 初始参数
+num_iterations = 200;  % 迭代次数
+num_teams = 20;  % 种群数量
+num_dims = 2;  % 问题的维度
+w = 0.8;  % 惯性因子
+c1 = 2;  % 学习因子1
+c2 = 2;  % 学习因子2
+search_space_lower_bound = [-1, -1];  % 搜索空间的下界
+search_space_upper_bound = [1, 1];  % 搜索空间的上界
+
+% PSO优化
+[gbest, gbest_value] = PSO_optimize(num_iterations, num_teams, num_dims, @error_function, w, c1, c2, search_space_lower_bound, search_space_upper_bound);
+
+% 输出最佳控制增益
+fprintf('最佳控制增益 EmuD1 = %f, EmuD2 = %f\n', gbest(1), gbest(2));
+fprintf('最小误差 = %f\n', gbest_value(end));
+
+% 使用最佳控制增益运行系统模拟并绘制y1和y2
 A = [2, 1, 1; 
      1, 1, 0;
      0, 1, 2];
@@ -10,37 +29,24 @@ C = [1, 0, 0;
 x = zeros(3, 1);
 r = [1; 1];
 
-% 计算反馈增益K 和 参考增益F
-EmuD1 = 0;
-EmuD2 = 0;
-[K,F] = Axui_FeedbackGain(EmuD1, EmuD2);
+[K, F] = Axui_FeedbackGain(gbest(1), gbest(2));
 
 T = 0.01;
 num_steps = 1000;
-
-% 用于存储每个时间步的输出
 y = zeros(2, num_steps);
-
-% 模拟
 for k = 1:num_steps
-    % 使用控制器计算输入
     u = -K*x + F*r;
-    % 更新系统状态
     x = T * (A * x + B * u) + x;
-    % 计算并存储输出
     y(:, k) = C * x;
 end
 
-% 创建时间向量
 t = (0:num_steps-1) * T;
-
-% 绘制输出
 figure;
 plot(t, y(1, :), 'DisplayName', 'y1');
 hold on;
 plot(t, y(2, :), 'DisplayName', 'y2');
 hold off;
-title('Outputs y1 and y2 over time');
-xlabel('Time');
-ylabel('Output');
-legend;
+title('使用最优增益的y1和y2随时间变化图');
+xlabel('时间');
+ylabel('输出');
+legend('y1', 'y2');
